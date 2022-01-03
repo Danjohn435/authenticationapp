d462271eaa58d98c4af309db7ab99d059d95821b
Dan <danjohn.hill@gmail.com>
2022-01-02 20:23:03 -0700

    added user model

---
+++ app/src/main/java/com/example/helloworld/model/User.kt
@@ -0,0 +1,9 @@
+package com.example.helloworld.model
+
+data class User(
+    val _id: String,
+    val email: String,
+    val firstName: String,
+    val lastName: String,
+    val authToken: String
+)
