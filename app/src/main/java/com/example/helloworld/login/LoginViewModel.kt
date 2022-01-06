09589f91e3b02daf24f0cd868dde3aeecd6e2e43
Dan <danjohn.hill@gmail.com>
2022-01-05 21:11:45 -0700

    added factory and implimented pattern

--- app/src/main/java/com/example/helloworld/login/LoginViewModel.kt
+++ app/src/main/java/com/example/helloworld/login/LoginViewModel.kt
@@ -3,5 +3,18 @@ package com.example.helloworld.login
 import androidx.lifecycle.ViewModel
 
 class LoginViewModel : ViewModel() {
+    fun handleEmailChanged(email: String) {
+
+    }
+
+    fun handlePasswordChanged(password: String) {
+    }
+
+    fun handleLoginButtonClick() {
+    }
+
+    fun handleRegisterButtonClick() {
+
+    }
     // TODO: Implement the ViewModel
 }
